6f1789e3ced931371dd1d9dad7e16f326e32996d
Facassanxt <asteblo@gmail.com>
2020-02-10 15:54:19 +0300

    LabHello

---
+++ labHello/labHello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Имя?");
+            string x = Console.ReadLine();
+            Console.Write("Фамилия?");
+            string y = Console.ReadLine();
+            Console.Write("Привет, {0}{1}!", x, y);
+        }
+    }
+}
