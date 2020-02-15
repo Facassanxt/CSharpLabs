f6a5c2d1f1dfcbe406fe0f38b022b8a5de50848e
Facassanxt <asteblo@gmail.com>
2020-02-15 10:55:00 +0300

    split

--- labHello/labHello/Program.cs
+++ labHello/labHello/Program.cs
@@ -21,6 +21,17 @@ namespace labHello
                 Console.WriteLine("Привет, {0}{1}!", x, y);
             }
 
+            string phrase = "The quick brown    fox     jumps over the lazy dog.";
+            string[] words = phrase.Split(' ');
+
+            Console.Write(words[0]);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+                System.Console.WriteLine($"<{word}>");
+            }
+
             Console.ReadKey();
         }
     }
