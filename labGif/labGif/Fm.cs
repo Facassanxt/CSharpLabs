f889b384fd55b597c45eb8e7f507374b7d4e386f
Facassanxt <asteblo@gmail.com>
2020-02-17 15:11:38 +0300

    labGif

---
+++ labGif/labGif/Fm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labGif
+{
+    public partial class labGif : Form
+    {
+        public labGif()
+        {
+            InitializeComponent();
+            button1.Click += delegate
+            {
+                pictureBox1.Enabled = !pictureBox1.Enabled;
+            };
+
+        }
+
+    }
+}
