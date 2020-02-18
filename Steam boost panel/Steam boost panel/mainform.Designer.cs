d42fa60a2cb30d405eddd31942ddf3dce5e5d824
Алексей Стебло <asteblo@gmail.com>
2020-02-18 03:40:02 +0300

    transparent border and panel movement

---
+++ Steam boost panel/Steam boost panel/mainform.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Steam_boost_panel
+{
+    partial class mainform
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.Panel_header = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = null;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.Location = new System.Drawing.Point(0, 0);
+            this.metroLabel1.Margin = new System.Windows.Forms.Padding(0);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(800, 450);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "MOUSE MOVE";
+            this.metroLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Panel_header
+            // 
+            this.Panel_header.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Panel_header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Panel_header.Location = new System.Drawing.Point(0, 0);
+            this.Panel_header.Name = "Panel_header";
+            this.Panel_header.Size = new System.Drawing.Size(800, 49);
+            this.Panel_header.TabIndex = 1;
+            // 
+            // mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Panel_header);
+            this.Controls.Add(this.metroLabel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "mainform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.Panel Panel_header;
+    }
+}
+
---
+++ Steam boost panel/Steam boost panel/mainform.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steam_boost_panel
+{
+    public partial class mainform : Form
+    {
+        bool drag = false;
+        Point start_point = new Point(0, 0);
+
+        public mainform()
+        {
+            InitializeComponent();
+            Panel_header.MouseDown += Panel_header_MouseDown;
+            Panel_header.MouseMove += Panel_header_MouseMove;
+            Panel_header.MouseUp += Panel_header_MouseUp;
+        }
+
+        private void Panel_header_MouseUp(object sender, MouseEventArgs e)
+        {
+            drag = false;
+            start_point = new Point(e.X, e.Y);
+        }
+
+        private void Panel_header_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drag)
+            {
+                Point p = PointToScreen(e.Location);
+                this.Location = new Point(p.X - start_point.X, p.Y - start_point.Y);
+            }
+        }
+
+        private void Panel_header_MouseDown(object sender, MouseEventArgs e)
+        {
+            drag = true;
+            start_point = new Point(e.X, e.Y);
+        }
+    }
+}
