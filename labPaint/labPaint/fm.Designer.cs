b6915d0a70b4bea9e311adc3e60ca3ab709168e1
Facassanxt <asteblo@gmail.com>
2020-04-17 15:56:16 +0300

    17.04.2020

---
+++ labPaint/labPaint/fm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace labPaint
+{
+    partial class fm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pamenu = new System.Windows.Forms.Panel();
+            this.paImage = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pamenu
+            // 
+            this.pamenu.Location = new System.Drawing.Point(12, 12);
+            this.pamenu.Name = "pamenu";
+            this.pamenu.Size = new System.Drawing.Size(168, 426);
+            this.pamenu.TabIndex = 0;
+            // 
+            // paImage
+            // 
+            this.paImage.Location = new System.Drawing.Point(186, 12);
+            this.paImage.Name = "paImage";
+            this.paImage.Size = new System.Drawing.Size(611, 426);
+            this.paImage.TabIndex = 1;
+            // 
+            // fm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.paImage);
+            this.Controls.Add(this.pamenu);
+            this.Name = "fm";
+            this.Text = "labPaint";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pamenu;
+        private System.Windows.Forms.Panel paImage;
+    }
+}
+
---
+++ labPaint/labPaint/fm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labPaint
+{
+    public partial class fm : Form
+    {
+        private bool isPressed;
+        private Bitmap b;
+
+        public fm()
+        {
+            InitializeComponent();
+
+            b = new Bitmap(paImage.Width, paImage.Height);
+            paImage.MouseDown += PaImage_MouseDown;
+            paImage.MouseUp += PaImage_MouseUp;
+            paImage.MouseMove += PaImage_MouseMove;
+            paImage.Paint += PaImage_Paint;
+        }
+
+        private void PaImage_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(b, new Point(0, 0));
+        }
+
+        private void PaImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isPressed) return;
+            var g = Graphics.FromImage(b);
+            g.DrawLine(new Pen(Color.Red, 5), e.X - 10, e.Y - 10, e.X + 10, e.Y + 10);
+            g.DrawLine(new Pen(Color.Red, 5), e.X - 10, e.Y + 10, e.X + 10, e.Y - 10);
+            g.Dispose();
+            paImage.CreateGraphics().DrawImage(b, new Point(0, 0));
+        }
+
+        private void PaImage_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPressed = false;
+        }
+
+        private void PaImage_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                isPressed = true;
+        }
+    }
+}
