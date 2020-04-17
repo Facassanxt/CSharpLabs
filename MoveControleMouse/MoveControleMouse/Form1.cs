7a032227166b7b515c55aba6b78755501f60617c
Facassanxt <asteblo@gmail.com>
2020-04-17 12:39:26 +0300

    17.04

--- MoveControleMouse/MoveControleMouse/Form1.cs
+++ MoveControleMouse/MoveControleMouse/Form1.cs
@@ -23,6 +23,20 @@ namespace MoveControleMouse
 
             pictureBox2.MouseDown += All_MouseDown;
             pictureBox2.MouseMove += All_MouseMove;
+
+            pictureBox1.MouseWheel += PictureBox1_MouseWheel;
+        }
+
+        private void PictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            int xDelta = e.Delta > 0 ? 2 : -2;
+            if (sender is Control x) 
+            {
+                x.Location = new Point(x.Location.X + xDelta*-1, x.Location.Y + xDelta*-1);
+                x.Width += xDelta * 2;
+                x.Height += xDelta * 2;
+
+            }
         }
 
         private void All_MouseMove(object sender, MouseEventArgs e)
