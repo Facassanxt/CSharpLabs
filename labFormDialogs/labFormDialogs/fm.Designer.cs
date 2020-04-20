88dfbbceb510b9137ece3fd86f6ba9267492245b
Facassanxt <asteblo@gmail.com>
2020-04-20 13:09:00 +0300

    labFormDialogs

---
+++ labFormDialogs/labFormDialogs/fm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace labFormDialogs
+{
+    partial class fm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.OpenDialog = new System.Windows.Forms.TextBox();
+            this.buDialog = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // OpenDialog
+            // 
+            this.OpenDialog.Location = new System.Drawing.Point(12, 33);
+            this.OpenDialog.Name = "OpenDialog";
+            this.OpenDialog.Size = new System.Drawing.Size(633, 20);
+            this.OpenDialog.TabIndex = 0;
+            // 
+            // buDialog
+            // 
+            this.buDialog.Location = new System.Drawing.Point(651, 30);
+            this.buDialog.Name = "buDialog";
+            this.buDialog.Size = new System.Drawing.Size(137, 23);
+            this.buDialog.TabIndex = 1;
+            this.buDialog.Text = "OpenDialog";
+            this.buDialog.UseVisualStyleBackColor = true;
+            // 
+            // fm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buDialog);
+            this.Controls.Add(this.OpenDialog);
+            this.Name = "fm";
+            this.Text = "labFormDialogs";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox OpenDialog;
+        private System.Windows.Forms.Button buDialog;
+    }
+}
+
---
+++ labFormDialogs/labFormDialogs/fm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labFormDialogs
+{
+    public partial class fm : Form
+    {
+        public fm()
+        {
+            InitializeComponent();
+            buDialog.Click += BuDialog_Click;
+        }
+
+        private void BuDialog_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.InitialDirectory = @"C:\";
+            openFileDialog.InitialDirectory = OpenDialog.Text;
+            openFileDialog.Filter = "txt files (*.txt)|*.txt";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                OpenDialog.Text = openFileDialog.FileName;
+            }
+        }
+    }
+}
