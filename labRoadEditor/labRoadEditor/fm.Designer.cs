d0598068fb2aaf8a879f3756916b138f68e9d918
Facassanxt <asteblo@gmail.com>
2020-04-23 15:37:26 +0300

    Добавлена работа с INI-файлом для загрузки карты

--- labRoadEditor/labRoadEditor/fm.Designer.cs
+++ labRoadEditor/labRoadEditor/fm.Designer.cs
@@ -28,20 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Download = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Download
+            // 
+            this.Download.AutoSize = true;
+            this.Download.BackColor = System.Drawing.Color.Transparent;
+            this.Download.FlatAppearance.BorderSize = 0;
+            this.Download.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Download.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Download.Location = new System.Drawing.Point(261, 24);
+            this.Download.Name = "Download";
+            this.Download.Size = new System.Drawing.Size(121, 39);
+            this.Download.TabIndex = 23;
+            this.Download.Text = "Загрузить";
+            this.Download.UseVisualStyleBackColor = false;
+            // 
+            // Save
+            // 
+            this.Save.AutoSize = true;
+            this.Save.BackColor = System.Drawing.Color.Transparent;
+            this.Save.FlatAppearance.BorderSize = 0;
+            this.Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Save.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Save.ForeColor = System.Drawing.Color.Black;
+            this.Save.Location = new System.Drawing.Point(134, 24);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(127, 39);
+            this.Save.TabIndex = 22;
+            this.Save.Text = "Сохранить";
+            this.Save.UseVisualStyleBackColor = false;
             // 
             // fm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(890, 623);
+            this.Controls.Add(this.Download);
+            this.Controls.Add(this.Save);
             this.Name = "fm";
             this.Text = "labRoadEditor";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button Download;
+        private System.Windows.Forms.Button Save;
     }
 }
 
--- labRoadEditor/labRoadEditor/fm.cs
+++ labRoadEditor/labRoadEditor/fm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,123 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace labRoadEditor
 {
-    public partial class fm : Form
+    public partial class fm : MaterialForm
     {
+        private int CountCFGSave = 0;
         public fm()
         {
             InitializeComponent();
+            var skinManager = MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            skinManager.ColorScheme = new ColorScheme(Primary.BlueGrey700, Primary.BlueGrey900, Primary.Blue50, Accent.Lime400, TextShade.WHITE);
+            Save.Click += Save_Click;
+            Download.Click += Download_Click;
+            StartForm();
         }
+
+        private void StartForm()
+        {
+            //toolsBar.Location = new Point(0, 64);
+            //toolsBar.Height = this.Height - 64;
+            //instpanel.Location = new Point(toolsBar.Width, 64);
+            //instpanel.Height = 60;
+            //instpanel.Width = this.Width - toolsBar.Width - 1;
+            //paImage.Location = new Point(toolsBar.Width, instpanel.Location.Y + instpanel.Height);
+            //paImage.Height = this.Height - 64 - instpanel.Height - 1;
+            //paImage.Width = this.Width - toolsBar.Width - 1;
+
+
+            Size resolution = Screen.PrimaryScreen.Bounds.Size;
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog MyDialog = new SaveFileDialog();
+            MyDialog.FileName = $"SavePaint_{CountCFGSave}";
+            MyDialog.Filter = $"png (*.png)|*.png";
+            if (MyDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            try
+            {
+                //string path = MyDialog.FileName;
+                //b.Save(path, System.Drawing.Imaging.ImageFormat.Png);
+                //CountFileSave++;
+            }
+            catch
+            {
+                DialogResult rezult = MessageBox.Show("Невозможно сохранить файл",
+                "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Download_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String fullPath = Application.StartupPath.ToString();
+                string[] words = fullPath.Split(new char[] { '\\' });
+                fullPath = "";
+                for (int i = 0; i < words.Length-3; i++)
+                {
+                    fullPath += words[i] + '\\';
+                }
+                INIManager manager = new INIManager($"{fullPath}\\cfg\\MapRoad.ini");
+                string name = manager.GetPrivateString("Facassanxt", "0");
+                MessageBox.Show(name);
+                manager.WritePrivateString("Facassanxt", "0", "164482754792524");
+                name = manager.GetPrivateString("Facassanxt", "0");
+                MessageBox.Show(name);
+            }
+            catch (Exception)
+            {
+                DialogResult rezult = MessageBox.Show("Невозможно cчитать CFG",
+                "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //OpenFileDialog open_dialog = new OpenFileDialog();
+            //open_dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            //if (open_dialog.ShowDialog() == DialogResult.OK)
+            //{
+            //    try
+            //    {
+            //        //
+            //    }
+            //    catch
+            //    {
+            //        DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+            //        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    }
+            //}
+        }
+    }
+    public class INIManager
+    {
+        public INIManager(string aPath)
+        {
+            path = aPath;
+        }
+        public INIManager() : this("") { }
+        public string GetPrivateString(string aSection, string aKey)
+        {
+            StringBuilder buffer = new StringBuilder(SIZE);
+            GetPrivateString(aSection, aKey, null, buffer, SIZE, path);
+            return buffer.ToString();
+        }
+        public void WritePrivateString(string aSection, string aKey, string aValue)
+        {
+            WritePrivateString(aSection, aKey, aValue, path);
+        }
+        public string Path { get { return path; } set { path = value; } }
+
+        private const int SIZE = 1024;
+        private string path = null;
+        [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileString")]
+        private static extern int GetPrivateString(string section, string key, string def, StringBuilder buffer, int size, string path);
+        [DllImport("kernel32.dll", EntryPoint = "WritePrivateProfileString")]
+        private static extern int WritePrivateString(string section, string key, string str, string path);
     }
 }
