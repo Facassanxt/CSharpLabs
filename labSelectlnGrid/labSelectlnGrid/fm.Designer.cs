86bf594bc9b90db929b2548be45a4d63883d1ed6
Алексей Стебло <asteblo@gmail.com>
2020-05-11 13:48:06 +0300

    1

--- labSelectlnGrid/labSelectlnGrid/fm.Designer.cs
+++ labSelectlnGrid/labSelectlnGrid/fm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace labSelectlnGrid
+﻿namespace labImageSelectlnGrid
 {
     partial class fm
     {
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "fm";
-            this.Text = "labSelectlnGrid";
+            this.Text = "labImageSelectlnGrid";
             this.ResumeLayout(false);
 
         }
--- labSelectlnGrid/labSelectlnGrid/fm.cs
+++ labSelectlnGrid/labSelectlnGrid/fm.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace labSelectlnGrid
+namespace labImageSelectlnGrid
 {
     public partial class fm : Form
     {
