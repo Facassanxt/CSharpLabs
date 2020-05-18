30b110c64e298df2a27035b7f8134cf65bd05d4c
Алексей Стебло <asteblo@gmail.com>
2020-05-18 15:51:36 +0300

    labFileExplorer

--- labFileExplorer/labFileExplorer/Form1.Designer.cs
+++ labFileExplorer/labFileExplorer/Form1.Designer.cs
@@ -28,26 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
-            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.buBack = new System.Windows.Forms.ToolStripButton();
+            this.buForward = new System.Windows.Forms.ToolStripButton();
+            this.buUp = new System.Windows.Forms.ToolStripButton();
+            this.edDir = new System.Windows.Forms.ToolStripTextBox();
+            this.buDirSelect = new System.Windows.Forms.ToolStripButton();
             this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.miViewLargeIcon = new System.Windows.Forms.ToolStripMenuItem();
+            this.miViewSmallIcon = new System.Windows.Forms.ToolStripMenuItem();
+            this.miViewList = new System.Windows.Forms.ToolStripMenuItem();
+            this.miViewDetails = new System.Windows.Forms.ToolStripMenuItem();
+            this.miViewTile = new System.Windows.Forms.ToolStripMenuItem();
+            this.LV = new System.Windows.Forms.ListView();
+            this.iLargeIcons = new System.Windows.Forms.ImageList(this.components);
+            this.iSmallIcons = new System.Windows.Forms.ImageList(this.components);
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton1,
-            this.toolStripButton2,
-            this.toolStripButton3,
-            this.toolStripTextBox1,
-            this.toolStripButton4,
+            this.buBack,
+            this.buForward,
+            this.buUp,
+            this.edDir,
+            this.buDirSelect,
             this.toolStripDropDownButton1});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
@@ -55,72 +63,125 @@
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
-            // toolStripButton1
+            // buBack
             // 
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton1.Text = "toolStripButton1";
+            this.buBack.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.buBack.Image = ((System.Drawing.Image)(resources.GetObject("buBack.Image")));
+            this.buBack.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buBack.Name = "buBack";
+            this.buBack.Size = new System.Drawing.Size(23, 22);
+            this.buBack.Text = "◀";
             // 
-            // toolStripButton2
+            // buForward
             // 
-            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
-            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton2.Text = "toolStripButton2";
+            this.buForward.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.buForward.Image = ((System.Drawing.Image)(resources.GetObject("buForward.Image")));
+            this.buForward.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buForward.Name = "buForward";
+            this.buForward.Size = new System.Drawing.Size(23, 22);
+            this.buForward.Text = "▶";
             // 
-            // toolStripButton3
+            // buUp
             // 
-            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
-            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton3.Text = "toolStripButton3";
+            this.buUp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.buUp.Image = ((System.Drawing.Image)(resources.GetObject("buUp.Image")));
+            this.buUp.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buUp.Name = "buUp";
+            this.buUp.Size = new System.Drawing.Size(23, 22);
+            this.buUp.Text = "▲";
             // 
-            // toolStripTextBox1
+            // edDir
             // 
-            this.toolStripTextBox1.Name = "toolStripTextBox1";
-            this.toolStripTextBox1.Size = new System.Drawing.Size(450, 25);
+            this.edDir.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.edDir.Name = "edDir";
+            this.edDir.Size = new System.Drawing.Size(450, 25);
             // 
-            // toolStripButton4
+            // buDirSelect
             // 
-            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
-            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton4.Name = "toolStripButton4";
-            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton4.Text = "toolStripButton4";
+            this.buDirSelect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.buDirSelect.Image = ((System.Drawing.Image)(resources.GetObject("buDirSelect.Image")));
+            this.buDirSelect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buDirSelect.Name = "buDirSelect";
+            this.buDirSelect.Size = new System.Drawing.Size(23, 22);
+            this.buDirSelect.Text = "...";
             // 
             // toolStripDropDownButton1
             // 
             this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miViewLargeIcon,
+            this.miViewSmallIcon,
+            this.miViewList,
+            this.miViewDetails,
+            this.miViewTile});
             this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
             this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
             this.toolStripDropDownButton1.Size = new System.Drawing.Size(40, 22);
             this.toolStripDropDownButton1.Text = "Вид";
             // 
-            // listView1
+            // miViewLargeIcon
             // 
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(0, 25);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(800, 425);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.miViewLargeIcon.Name = "miViewLargeIcon";
+            this.miViewLargeIcon.Size = new System.Drawing.Size(180, 22);
+            this.miViewLargeIcon.Text = "LargeIcon";
+            // 
+            // miViewSmallIcon
+            // 
+            this.miViewSmallIcon.Name = "miViewSmallIcon";
+            this.miViewSmallIcon.Size = new System.Drawing.Size(180, 22);
+            this.miViewSmallIcon.Text = "SmallIcon";
+            // 
+            // miViewList
+            // 
+            this.miViewList.Name = "miViewList";
+            this.miViewList.Size = new System.Drawing.Size(180, 22);
+            this.miViewList.Text = "List";
+            // 
+            // miViewDetails
+            // 
+            this.miViewDetails.Name = "miViewDetails";
+            this.miViewDetails.Size = new System.Drawing.Size(180, 22);
+            this.miViewDetails.Text = "Details";
+            // 
+            // miViewTile
+            // 
+            this.miViewTile.Name = "miViewTile";
+            this.miViewTile.Size = new System.Drawing.Size(180, 22);
+            this.miViewTile.Text = "Tile";
+            // 
+            // LV
+            // 
+            this.LV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LV.HideSelection = false;
+            this.LV.LargeImageList = this.iLargeIcons;
+            this.LV.Location = new System.Drawing.Point(0, 25);
+            this.LV.Name = "LV";
+            this.LV.Size = new System.Drawing.Size(800, 425);
+            this.LV.SmallImageList = this.iSmallIcons;
+            this.LV.TabIndex = 1;
+            this.LV.UseCompatibleStateImageBehavior = false;
+            // 
+            // iLargeIcons
+            // 
+            this.iLargeIcons.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("iLargeIcons.ImageStream")));
+            this.iLargeIcons.TransparentColor = System.Drawing.Color.Transparent;
+            this.iLargeIcons.Images.SetKeyName(0, "799655_folder_512x512.png");
+            this.iLargeIcons.Images.SetKeyName(1, "75_-Folder_Content-_document_paper_write_note-128.png");
+            // 
+            // iSmallIcons
+            // 
+            this.iSmallIcons.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("iSmallIcons.ImageStream")));
+            this.iSmallIcons.TransparentColor = System.Drawing.Color.Transparent;
+            this.iSmallIcons.Images.SetKeyName(0, "799655_folder_512x512.png");
+            this.iSmallIcons.Images.SetKeyName(1, "75_-Folder_Content-_document_paper_write_note-128.png");
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.LV);
             this.Controls.Add(this.toolStrip1);
             this.Name = "Form1";
             this.Text = "labFileExplorer";
@@ -134,13 +195,20 @@
         #endregion
 
         private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton toolStripButton1;
-        private System.Windows.Forms.ToolStripButton toolStripButton2;
-        private System.Windows.Forms.ToolStripButton toolStripButton3;
-        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
-        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton buBack;
+        private System.Windows.Forms.ToolStripButton buForward;
+        private System.Windows.Forms.ToolStripButton buUp;
+        private System.Windows.Forms.ToolStripTextBox edDir;
+        private System.Windows.Forms.ToolStripButton buDirSelect;
         private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView LV;
+        private System.Windows.Forms.ToolStripMenuItem miViewLargeIcon;
+        private System.Windows.Forms.ToolStripMenuItem miViewSmallIcon;
+        private System.Windows.Forms.ToolStripMenuItem miViewList;
+        private System.Windows.Forms.ToolStripMenuItem miViewDetails;
+        private System.Windows.Forms.ToolStripMenuItem miViewTile;
+        private System.Windows.Forms.ImageList iLargeIcons;
+        private System.Windows.Forms.ImageList iSmallIcons;
     }
 }
 
--- labFileExplorer/labFileExplorer/Form1.cs
+++ labFileExplorer/labFileExplorer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,103 @@ namespace labFileExplorer
 {
     public partial class Form1 : Form
     {
+        private string _curDir;
+
+        public string CurDir 
+        { 
+            get
+            {
+                return _curDir;
+            }
+            private set 
+            {
+                _curDir = value;
+                edDir.Text = value;
+            }  
+        }
+        public string SelItem { get; private set; }
+
         public Form1()
         {
             InitializeComponent();
+            //CurDir = "D:\\";
+            CurDir = Directory.GetCurrentDirectory();
+
+
+
+            buUp.Click += (s, e) => LoadDir(Directory.GetParent(CurDir).ToString());
+            edDir.KeyDown += EdDir_KeyDown;
+
+            miViewLargeIcon.Click += (s, e) => LV.View = View.LargeIcon;
+            miViewSmallIcon.Click += (s, e) => LV.View = View.SmallIcon;
+            miViewList.Click += (s, e) => LV.View = View.List;
+            miViewDetails.Click += (s, e) => LV.View = View.Details;
+            miViewTile.Click += (s, e) => LV.View = View.Tile;
+
+            buDirSelect.Click += BuDirSelect_Click;
+
+
+            LV.ItemSelectionChanged += (s, e) => SelItem = Path.Combine(CurDir,e.Item.Text);
+            LV.DoubleClick += (s, e) => LoadDir(SelItem);
+
+            LV.Columns.Add("Имя", 200);
+            LV.Columns.Add("Дата изменения", 150);
+            LV.Columns.Add("Тип", 100);
+            LV.Columns.Add("Размер", 150);
+
+
+            Text += " : Drivers=" + string.Join(" ", Directory.GetLogicalDrives());
+
+            LoadDir(CurDir);
+
+            /* TODO HW
+             * По расширению файла менять картинку  
+                //f.Extension
+             *  Добавить меню выбора размера файла (Кб,Мб)
+             *   Выбор диска
+             *   Больше инфы
+             *   
+             */
+
+        }
+
+        private void BuDirSelect_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                LoadDir(dialog.SelectedPath);
+            }
+        }
+
+        private void EdDir_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                LoadDir(edDir.Text);
+            }
+        }
+
+        private void LoadDir(string newDir)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(newDir);
+            LV.BeginUpdate();
+            LV.Items.Clear();
+            foreach (var item in directoryInfo.GetDirectories())
+            {
+                var f = new FileInfo(item.FullName);
+                //LV.Items.Add(item.Name, 0);
+                LV.Items.Add(new ListViewItem(new string[] { item.Name, f.LastWriteTime.ToString(), "Папка", ""}, 0));
+
+            }
+            foreach (var item in directoryInfo.GetFiles())
+            {
+                var f = new FileInfo(item.FullName);
+                //LV.Items.Add(item.Name, 1);
+                LV.Items.Add(new ListViewItem(new string[] { item.Name, f.LastWriteTime.ToString(), "Файл", f.Length.ToString() + " байт" }, 1));
+            }
+            LV.EndUpdate();
+            CurDir = newDir;
         }
     }
 }
