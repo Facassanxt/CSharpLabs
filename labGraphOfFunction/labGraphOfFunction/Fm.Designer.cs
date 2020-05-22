0fcfa22ea2476ecb914322f06c589fb03be78246
Алексей Стебло <asteblo@gmail.com>
2020-05-22 19:10:30 +0300

    labGraphOfFunction

---
+++ labGraphOfFunction/labGraphOfFunction/Fm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace labGraphOfFunction
+{
+    partial class Fm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PiGraph = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PiGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PiGraph
+            // 
+            this.PiGraph.BackColor = System.Drawing.Color.Transparent;
+            this.PiGraph.Location = new System.Drawing.Point(195, 64);
+            this.PiGraph.Margin = new System.Windows.Forms.Padding(2);
+            this.PiGraph.Name = "PiGraph";
+            this.PiGraph.Size = new System.Drawing.Size(1206, 825);
+            this.PiGraph.TabIndex = 2;
+            this.PiGraph.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.RosyBrown;
+            this.button1.Location = new System.Drawing.Point(21, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Fm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1400, 900);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PiGraph);
+            this.Name = "Fm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "labGraphOfFunction";
+            ((System.ComponentModel.ISupportInitialize)(this.PiGraph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PiGraph;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ labGraphOfFunction/labGraphOfFunction/Fm.cs
@@ -0,0 +1,128 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labGraphOfFunction
+{
+    public partial class Fm : MaterialForm
+    {
+        const int pix = 20;
+        private Bitmap Bmarkup;
+        public Fm()
+        {
+            InitializeComponent();
+            var skinManager = MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkinManager.Themes.DARK;
+            skinManager.ColorScheme = new ColorScheme(Primary.BlueGrey700, Primary.BlueGrey900, Primary.BlueGrey100, Accent.Red400, TextShade.WHITE);
+            Resize += Fm_Resize;
+            button1.Click += Button1_Click;
+            PiGraph.Paint += PiGraph_Paint;
+
+
+            Bmarkup = new Bitmap(Screen.PrimaryScreen.Bounds.Width*2, Screen.PrimaryScreen.Bounds.Height*2);
+            StartForm();
+        }
+
+        private void Fm_Resize(object sender, EventArgs e)
+        {
+            PiGraph.Height = Height - 64 - 2;
+            PiGraph.Width = Width * 4 / 5 - 2;
+            PiGraph.Location = new Point(Width - Width * 4 / 5, 64);
+
+            int w = PiGraph.Width / 2;
+            int h = PiGraph.Height / 2;
+            using (Graphics g = Graphics.FromImage(Bmarkup))
+            {
+                g.Clear(BackColor);
+                g.DrawImage(Bmarkup, 0, 0);
+                g.TranslateTransform(w, h);
+                DrawXAxis(new Point(-w, 0), new Point(w, 0), g);
+                DrawYAxis(new Point(0, h), new Point(0, -h), g);
+                g.FillEllipse(Brushes.Red, -2, -2, 4, 4);
+            }
+        }
+        private void PiGraph_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(Bmarkup, 0, 0);
+            e.Graphics.TranslateTransform(PiGraph.Width / 2, PiGraph.Height / 2);
+        }
+        private void DrawXAxis(Point start, Point end, Graphics g)
+        {
+            for (int i = pix; i < end.X; i += pix)
+            {
+                g.DrawLine(Pens.RosyBrown, i, -5, i, 5);
+                DrawText(new Point(i, 5), (i / pix).ToString(), g);
+                g.DrawLine(Pens.RosyBrown, -i, -5, -i, 5);
+                DrawText(new Point(-i, 5), (-i / pix).ToString(), g);
+            }
+            g.DrawLine(Pens.RosyBrown, start, end);
+        }
+
+        private void DrawYAxis(Point start, Point end, Graphics g)
+        {
+            for (int i = pix; i < start.Y; i += pix)
+            {
+                g.DrawLine(Pens.RosyBrown, -5, i, 5, i);
+                DrawText(new Point(5, i), (-i / pix).ToString(), g, true);
+                g.DrawLine(Pens.RosyBrown, -5, -i, 5, -i);
+                DrawText(new Point(5, -i), (i / pix).ToString(), g, true);
+            }
+            g.DrawLine(Pens.RosyBrown, start, end);
+        }
+
+        private void DrawText(Point point, string text, Graphics g, bool isYAxis = false)
+        {
+            var f = new Font(Font.FontFamily, 8);
+            var size = g.MeasureString(text, f);
+            var pt = isYAxis
+                ? new PointF(point.X + 1, point.Y - size.Height / 2)
+                : new PointF(point.X - size.Width / 2, point.Y + 1);
+            var rect = new RectangleF(pt, size);
+            g.DrawString(text, f, Brushes.Tomato, rect);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int w = PiGraph.Width / 2;
+            int h = PiGraph.Height / 2;
+            using (Graphics BG = Graphics.FromImage(Bmarkup))
+            {
+                BG.TranslateTransform(w, h);
+                for (float x = -30; x <= 30; x += 0.001F)
+                {
+                    float y = x * x;
+                    BG.FillEllipse(Brushes.White, x * 30, -y * 30, 2, 2);
+                }
+                PiGraph.Refresh();
+            }
+        }
+
+        private void StartForm()
+        {
+            this.Height = 1080;
+            this.Width = 1920;
+            PiGraph.Height = Height - 64 - 2;
+            PiGraph.Width = Width * 4 / 5 - 2;
+            PiGraph.Location = new Point(Width - Width * 4 / 5, 64);
+            int w = PiGraph.Width / 2;
+            int h = PiGraph.Height / 2;
+            using (Graphics g = Graphics.FromImage(Bmarkup))
+            {
+                g.Clear(BackColor);
+                g.TranslateTransform(w, h);
+                DrawXAxis(new Point(-w, 0), new Point(w, 0), g);
+                DrawYAxis(new Point(0, h), new Point(0, -h), g);
+                g.FillEllipse(Brushes.Red, -2, -2, 4, 4);
+            }
+        }
+    }
+}
