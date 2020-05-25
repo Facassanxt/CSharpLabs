0d6c1dd46752cccd975fe5c9e0502e389609921b
Алексей Стебло <asteblo@gmail.com>
2020-05-25 14:46:27 +0300

    labLINQext

---
+++ labLINQext/labLINQext/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace labLINQext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new string[] { "Москва", "Сочи", "Орел", "Самара", "Смоленск", "Ялта" };
+
+            var x1 = arr.Where(v => v.StartsWith("С")).Where(v => v.Length > 5).OrderBy(v => v).ToArray();
+            Console.WriteLine(string.Join(Environment.NewLine, x1));
+            Console.WriteLine();
+
+
+            var x2 = arr.OrderBy(v => v).Select(v => $"{v} - {v.Contains('а')}").ToArray();
+            Console.WriteLine(string.Join(Environment.NewLine, x2));
+
+        }
+    }
+}
