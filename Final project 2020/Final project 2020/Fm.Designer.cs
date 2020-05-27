b04eb5f55bee10d9cba83c02d5d9d4ea4d0f280e
Алексей Стебло <asteblo@gmail.com>
2020-05-27 20:57:35 +0300

    GameOfLife v1

---
+++ Final project 2020/Final project 2020/Fm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Final_project_2020
+{
+    partial class Fm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.buReset = new System.Windows.Forms.Button();
+            this.buPlay = new System.Windows.Forms.Button();
+            this.buStop = new System.Windows.Forms.Button();
+            this.gameScreen = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 500;
+            // 
+            // buReset
+            // 
+            this.buReset.AutoSize = true;
+            this.buReset.BackColor = System.Drawing.Color.Transparent;
+            this.buReset.FlatAppearance.BorderSize = 0;
+            this.buReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buReset.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buReset.ForeColor = System.Drawing.Color.Coral;
+            this.buReset.Location = new System.Drawing.Point(229, 29);
+            this.buReset.Name = "buReset";
+            this.buReset.Size = new System.Drawing.Size(121, 39);
+            this.buReset.TabIndex = 36;
+            this.buReset.Text = "Reset";
+            this.buReset.UseVisualStyleBackColor = false;
+            // 
+            // buPlay
+            // 
+            this.buPlay.AutoSize = true;
+            this.buPlay.BackColor = System.Drawing.Color.Transparent;
+            this.buPlay.FlatAppearance.BorderSize = 0;
+            this.buPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buPlay.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buPlay.ForeColor = System.Drawing.Color.Coral;
+            this.buPlay.Location = new System.Drawing.Point(338, 29);
+            this.buPlay.Name = "buPlay";
+            this.buPlay.Size = new System.Drawing.Size(121, 39);
+            this.buPlay.TabIndex = 37;
+            this.buPlay.Text = "Play";
+            this.buPlay.UseVisualStyleBackColor = false;
+            // 
+            // buStop
+            // 
+            this.buStop.AutoSize = true;
+            this.buStop.BackColor = System.Drawing.Color.Transparent;
+            this.buStop.FlatAppearance.BorderSize = 0;
+            this.buStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buStop.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buStop.ForeColor = System.Drawing.Color.Coral;
+            this.buStop.Location = new System.Drawing.Point(465, 29);
+            this.buStop.Name = "buStop";
+            this.buStop.Size = new System.Drawing.Size(121, 39);
+            this.buStop.TabIndex = 38;
+            this.buStop.Text = "Stop";
+            this.buStop.UseVisualStyleBackColor = false;
+            // 
+            // gameScreen
+            // 
+            this.gameScreen.Location = new System.Drawing.Point(1, 62);
+            this.gameScreen.Name = "gameScreen";
+            this.gameScreen.Size = new System.Drawing.Size(1197, 784);
+            this.gameScreen.TabIndex = 39;
+            // 
+            // Fm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 898);
+            this.Controls.Add(this.gameScreen);
+            this.Controls.Add(this.buStop);
+            this.Controls.Add(this.buPlay);
+            this.Controls.Add(this.buReset);
+            this.Name = "Fm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GameOfLife";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button buReset;
+        private System.Windows.Forms.Button buPlay;
+        private System.Windows.Forms.Button buStop;
+        private System.Windows.Forms.Panel gameScreen;
+    }
+}
+
---
+++ Final project 2020/Final project 2020/Fm.cs
@@ -0,0 +1,137 @@
+﻿using Final_project_2020.Properties;
+using GameOfLife;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_project_2020
+{
+    public partial class Fm : MaterialForm
+    {
+        private Engine engine = null;
+        private static readonly Color aliveCell = Color.DimGray;
+        private static readonly Color deadCell = Color.LightSlateGray;
+        private const int cellSize = 40;
+        private const int screenSize = 900;
+
+        public Fm()
+        {
+            InitializeComponent();
+            var skinManager = MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkinManager.Themes.DARK;
+            skinManager.ColorScheme = new ColorScheme(Primary.BlueGrey700, Primary.BlueGrey900, Primary.BlueGrey100, Accent.Red400, TextShade.WHITE);
+
+            buPlay.Click += buPlay_Click;
+            buReset.Click += buReset_Click;
+            buStop.Click += buStop_Click;
+            timer.Tick += timer_Tick;
+
+            Resize += Fm_Resize;
+            Load += Fm_Load;
+            startForm();
+        }
+
+        private void startForm()
+        {
+            this.Height = Screen.PrimaryScreen.Bounds.Height / 3 * 2;
+            this.Width = Screen.PrimaryScreen.Bounds.Width / 3 * 2;
+            gameScreen.Height = Height - 64 - 2;
+            gameScreen.Width = Width - 4;
+
+            engine = new Engine(gameScreen.Width / cellSize, gameScreen.Height / cellSize);
+
+
+            gameScreen.Location = new Point(2, 64);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            engine.Tick();
+            Text = $"GameOfLife : {engine.Ticks.ToString()}";
+            UpdateCells();
+        }
+
+        private void buPlay_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+            buPlay.Enabled = false;
+            buStop.Enabled = true;
+        }
+
+        private void buStop_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            buStop.Enabled = false;
+            buPlay.Enabled = true;
+        }
+
+        private void buReset_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            buStop.Enabled = false;
+            buPlay.Enabled = true;
+
+            engine = new Engine(engine.Height, engine.Width);
+            Text = $"GameOfLife : {engine.Ticks.ToString()}";
+
+            UpdateCells();
+        }
+
+        private void ClickCell(object sender, EventArgs e)
+        {
+            if (timer.Enabled)
+                return;
+
+            int buttonLinearIndex = gameScreen.Controls.IndexOf(sender as Control);
+            int y = buttonLinearIndex / engine.Width;
+            int x = buttonLinearIndex % engine.Width;
+
+            engine[y, x] = !engine[y, x];
+            ((Button)sender).BackColor = engine[y, x] ? aliveCell : deadCell;
+            ((Button)sender).BackgroundImage = engine[y, x] ? Resources.GlowStar_16x : Resources.Blank_Star;
+        }
+
+        private void UpdateCells()
+        {
+            for (int linearIndex = 0; linearIndex < gameScreen.Controls.Count; ++linearIndex)
+            {
+                gameScreen.Controls[linearIndex].BackColor =
+                    engine[linearIndex / engine.Width, linearIndex % engine.Width] ? aliveCell : deadCell;
+                gameScreen.Controls[linearIndex].BackgroundImage =
+                    engine[linearIndex / engine.Width, linearIndex % engine.Width] ? Resources.GlowStar_16x : Resources.Blank_Star;
+                gameScreen.Controls[linearIndex].BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            }
+
+        }
+
+        private void Fm_Load(object sender, EventArgs e)
+        {
+            for (int j = 0; j + cellSize <= gameScreen.Height; j += cellSize)
+                for (int i = 0; i + cellSize <= gameScreen.Width; i += cellSize)
+                {
+                    Button newButton = new Button();
+                    newButton.Size = new Size(cellSize, cellSize);
+                    newButton.Location = new Point(i, j);
+                    newButton.Click += ClickCell;
+                    gameScreen.Controls.Add(newButton);
+                }
+
+            UpdateCells();
+            buReset_Click(sender, e);
+        }
+
+        private void Fm_Resize(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
