aef2d0381c5a511d07b5d0752f3516a795c6221e
Алексей Стебло <asteblo@gmail.com>
2020-05-30 13:25:57 +0300

    Fix engine

--- Final project 2020/Final project 2020/Fm.cs
+++ Final project 2020/Final project 2020/Fm.cs
@@ -64,7 +64,7 @@ namespace Final_project_2020
             gameScreen.Height = Height - 66 - 2;
             screenSize = Width / 10 * 9;
             gameScreen.Width = screenSize - 4;
-            engine = new Engine(gameScreen.Height, screenSize/ cellSize);
+            engine = new Engine(gameScreen.Height / cellSize, screenSize/ cellSize);
             gameScreen.Location = new Point(Width/2 - screenSize/2, 66);
 
             MinimumSize = new Size(Width, Height);
@@ -126,7 +126,7 @@ namespace Final_project_2020
             buPlay.Enabled = true;
 
 
-            engine = new Engine(gameScreen.Height, screenSize / cellSize);
+            engine = new Engine(gameScreen.Height / cellSize, screenSize / cellSize);
             UpdateCells();
         }
 
