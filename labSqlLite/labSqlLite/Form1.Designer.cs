5a36cc79a54756ee3a96cd4fffa27aaaac676621
Алексей Стебло <asteblo@gmail.com>
2020-06-12 15:14:47 +0300

    SQL

--- labSqlLite/labSqlLite/Form1.Designer.cs
+++ labSqlLite/labSqlLite/Form1.Designer.cs
@@ -28,29 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.edSQL = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.buUsersShow = new System.Windows.Forms.Button();
+            this.buNotesShow = new System.Windows.Forms.Button();
+            this.buRunOne = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.edNotesCaption = new System.Windows.Forms.TextBox();
+            this.buNotesAdd = new System.Windows.Forms.Button();
+            this.edNotesPriority = new System.Windows.Forms.NumericUpDown();
             this.button5 = new System.Windows.Forms.Button();
+            this.lvLogs = new System.Windows.Forms.ListView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edNotesPriority)).BeginInit();
             this.SuspendLayout();
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(13, 39);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(344, 424);
-            this.textBox2.TabIndex = 1;
             // 
             // label1
             // 
@@ -61,15 +53,15 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Log";
             // 
-            // textBox1
+            // edNgh
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(363, 39);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(393, 101);
-            this.textBox1.TabIndex = 3;
+            this.edSQL.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.edSQL.Location = new System.Drawing.Point(363, 39);
+            this.edSQL.Multiline = true;
+            this.edSQL.Name = "edNgh";
+            this.edSQL.Size = new System.Drawing.Size(393, 101);
+            this.edSQL.TabIndex = 3;
+            this.edSQL.Text = "fdgfdgfd";
             // 
             // label2
             // 
@@ -82,36 +74,40 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Query";
             // 
-            // button1
+            // buUsersShow
             // 
-            this.button1.Location = new System.Drawing.Point(363, 146);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 33);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Пользователи";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buUsersShow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buUsersShow.Location = new System.Drawing.Point(363, 146);
+            this.buUsersShow.Name = "buUsersShow";
+            this.buUsersShow.Size = new System.Drawing.Size(102, 33);
+            this.buUsersShow.TabIndex = 5;
+            this.buUsersShow.Text = "Пользователи";
+            this.buUsersShow.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // buNotesShow
             // 
-            this.button2.Location = new System.Drawing.Point(471, 146);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 33);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Заметки";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buNotesShow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buNotesShow.Location = new System.Drawing.Point(471, 146);
+            this.buNotesShow.Name = "buNotesShow";
+            this.buNotesShow.Size = new System.Drawing.Size(75, 33);
+            this.buNotesShow.TabIndex = 6;
+            this.buNotesShow.Text = "Заметки";
+            this.buNotesShow.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(550, 146);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(81, 33);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Выполнить";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buRunOne.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buRunOne.Location = new System.Drawing.Point(550, 146);
+            this.buRunOne.Name = "button3";
+            this.buRunOne.Size = new System.Drawing.Size(81, 33);
+            this.buRunOne.TabIndex = 7;
+            this.buRunOne.Text = "Выполнить";
+            this.buRunOne.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(364, 186);
@@ -119,34 +115,35 @@
             this.dataGridView1.Size = new System.Drawing.Size(392, 252);
             this.dataGridView1.TabIndex = 8;
             // 
-            // textBox3
+            // edNotes
             // 
-            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.textBox3.Location = new System.Drawing.Point(363, 443);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(183, 20);
-            this.textBox3.TabIndex = 9;
+            this.edNotesCaption.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.edNotesCaption.Location = new System.Drawing.Point(363, 443);
+            this.edNotesCaption.Name = "edNotes";
+            this.edNotesCaption.Size = new System.Drawing.Size(183, 20);
+            this.edNotesCaption.TabIndex = 9;
             // 
-            // button4
+            // buNotesAdd
             // 
-            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button4.Location = new System.Drawing.Point(637, 442);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(119, 21);
-            this.button4.TabIndex = 10;
-            this.button4.Text = "+ Заметка";
-            this.button4.UseVisualStyleBackColor = true;
+            this.buNotesAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buNotesAdd.Location = new System.Drawing.Point(637, 442);
+            this.buNotesAdd.Name = "buNotesAdd";
+            this.buNotesAdd.Size = new System.Drawing.Size(119, 21);
+            this.buNotesAdd.TabIndex = 10;
+            this.buNotesAdd.Text = "+ Заметка";
+            this.buNotesAdd.UseVisualStyleBackColor = true;
             // 
-            // numericUpDown1
+            // ediuo
             // 
-            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.numericUpDown1.Location = new System.Drawing.Point(550, 444);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(81, 20);
-            this.numericUpDown1.TabIndex = 11;
+            this.edNotesPriority.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.edNotesPriority.Location = new System.Drawing.Point(550, 444);
+            this.edNotesPriority.Name = "ediuo";
+            this.edNotesPriority.Size = new System.Drawing.Size(81, 20);
+            this.edNotesPriority.TabIndex = 11;
             // 
             // button5
             // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button5.Location = new System.Drawing.Point(637, 147);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(119, 33);
@@ -154,46 +151,56 @@
             this.button5.Text = "Выполнить SQL";
             this.button5.UseVisualStyleBackColor = true;
             // 
+            // lvLogs
+            // 
+            this.lvLogs.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lvLogs.HideSelection = false;
+            this.lvLogs.Location = new System.Drawing.Point(12, 39);
+            this.lvLogs.Name = "lvLogs";
+            this.lvLogs.Size = new System.Drawing.Size(346, 425);
+            this.lvLogs.TabIndex = 13;
+            this.lvLogs.UseCompatibleStateImageBehavior = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(767, 471);
+            this.Controls.Add(this.lvLogs);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.edNotesPriority);
+            this.Controls.Add(this.buNotesAdd);
+            this.Controls.Add(this.edNotesCaption);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buRunOne);
+            this.Controls.Add(this.buNotesShow);
+            this.Controls.Add(this.buUsersShow);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.edSQL);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
             this.Name = "Form1";
             this.Text = "labSqlLite";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edNotesPriority)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox edSQL;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buUsersShow;
+        private System.Windows.Forms.Button buNotesShow;
+        private System.Windows.Forms.Button buRunOne;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox edNotesCaption;
+        private System.Windows.Forms.Button buNotesAdd;
+        private System.Windows.Forms.NumericUpDown edNotesPriority;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListView lvLogs;
     }
 }
 
--- labSqlLite/labSqlLite/Form1.cs
+++ labSqlLite/labSqlLite/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,67 @@ namespace labSqlLite
 {
     public partial class Form1 : Form
     {
+        private SQLiteConnection db;
         public Form1()
         {
             InitializeComponent();
+
+            db = new SQLiteConnection("myDB.db");
+            db.CreateTable<Logs>();
+            db.CreateTable<Users>();
+            db.CreateTable<Notes>();
+            db.Insert(new Logs() { DT = DateTime.Now });
+            lvLogs.Columns.Add("DateTime", 220);
+            lvLogs.View = View.Details;
+            foreach (var item in db.Table<Logs>())
+            {
+                lvLogs.Items.Add(item.DT.ToString());
+            }
+            buUsersShow.Click += (s, e) => dataGridView1.DataSource = db.Table<Users>().ToList();
+            buNotesShow.Click += (s, e) => dataGridView1.DataSource = db.Table<Notes>().ToList();
+
+            buNotesAdd.Click += BuNotesAdd_Click;
+            buRunOne.Click += (s, e) => MessageBox.Show(db.ExecuteScalar<int>(edSQL.Text).ToString());
+        }
+
+        /*TODO HW
+         * добавление, редактирование, удаление данных из таблицы
+         * обновление данных в таблице 
+         * 
+         * 
+         * 
+         */
+
+        private void BuNotesAdd_Click(object sender, EventArgs e)
+        {
+            var x = new Notes();
+            x.Caption = edNotesCaption.Text;
+            x.Priority = (byte)edNotesPriority.Value;
+            db.Insert(x);
+            dataGridView1.DataSource = db.Table<Notes>().ToList();
+        }
+
+        private class Logs
+        {
+            public DateTime DT { get; set; }
+        }
+
+        private class Users
+        {
+            [PrimaryKey, AutoIncrement]
+            public int ID { get; set; }
+            public string FIO { get; set; }
+            public string Email { get; set; }
+            public string Age { get; set; }
+
+        }
+
+        private class Notes
+        {
+            [PrimaryKey, AutoIncrement]
+            public int ID { get; set; }
+            public string Caption { get; set; }
+            public byte Priority { get; set; }
         }
     }
 }
